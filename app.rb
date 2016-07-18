b9d00283991f1038f37855ae8554f637f3aadebe
Indigo253931 <indigo253931@gmail.com>
2016-07-18 15:37:30 -0600

    gems

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
  require 'sinatra'
- require 'active_record' 
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ #require 'active_record' 
+#ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 # require './config/envrionments'
 # require './models/model'
 
  get '/' do 
  	"Hello World!"
  	#erb :index
- end
+end
